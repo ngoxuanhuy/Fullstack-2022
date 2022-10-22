0e58754fae5738199ad12562ba0bcf9f6af01c80
huy <ngoxuanhuy96@gmail.com>
2022-10-22 14:18:22 +0300

    Ex2.6: phonebook, step1

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    {name: 'Arto Hellas'}
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addNewName = (event) => {
+    event.preventDefault()
+    const newObj = {
+      name: newName
+    }
+    setPersons(persons.concat(newObj))
+    setNewName('')
+  }
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewName}>
+        <div>
+          Name: <input value={newName} onChange={handleNewName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, i) => <li key={i}>{person.name}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default App

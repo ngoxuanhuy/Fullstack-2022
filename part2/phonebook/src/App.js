6b51ddef26600dfb22ed96930b4b1d7b2be4b291
huy <ngoxuanhuy96@gmail.com>
2022-10-29 16:03:40 +0300

    Ex2.15 and Ex2.16: phonebook, step7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,7 +4,7 @@ import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
-import axios from 'axios'
+import personsService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -13,10 +13,10 @@ const App = () => {
   const [searchText, setSearchText] = useState('')
 
   useEffect(() => {
-    axios
-      .get("http://localhost:3001/persons")
-      .then(response => {
-        setPersons(response.data)
+    personsService
+      .getAll()
+      .then(returnedPersons => {
+        setPersons(returnedPersons)
       })
   }, [])
 
@@ -30,15 +30,12 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
     } else {
       // Add new person to the backend server
-      axios
-        .post("http://localhost:3001/persons", newObj)
-        .then(response => {
-          setPersons(persons.concat(response.data))
+      personsService
+        .create(newObj)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
           setNewName('')
           setNewPhoneNumber('')
-        })
-        .catch(err => {
-          alert(err)
         })
     }
   }

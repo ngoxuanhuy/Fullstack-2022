05cc34150a1136fe1751b3c7e5b49d49b84c18a6
huy <ngoxuanhuy96@gmail.com>
2022-10-31 12:46:42 +0200

    Ex3.6: phonebook backend, step6

--- part3/index.js
+++ part3/index.js
@@ -70,6 +70,12 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
+    if (persons.find(p => p.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
     const newContact = {
         name: body.name,
         number: body.number,
